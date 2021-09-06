5e656cd04571976ecb6c20069025062f45f19bec
Athanasios Mpekas <amp@thinx.gr>
2021-09-06 13:06:39 +0300

    initial

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=rnd2020;Initial Catalog=kath3;User ID=unisoft;Password=unisoft";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            MessageBox.Show("Connected!");
+
+            string sql = "SELECT ID,NAME,AFM FROM CUSTOMER";
+
+            var lc = connection.Query<Customer>(sql);
+
+            foreach(Customer c in lc)
+            {
+                listBox1.Items.Add(c.NAME);
+            }
+
+            connection.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+        }
+    }
+}
---
+++ WinFormsApp1/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Customer cust = new Customer();
+
+                
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            
+        }
+    }
+}
